c1ad5b784f1b42ff42517af24307a7efa412d26f
Al Jabir <87219339+Cyber-log@users.noreply.github.com>
2021-11-11 00:39:50 +0600

    Delete response.py

--- response.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def sample_responses(input_text):
-    user_massage = str(input_text).lower()
-
-    if user_massage in ("hello"):
-        return "Hey! How are you?"
-
-    return "Hey! Run /help"
